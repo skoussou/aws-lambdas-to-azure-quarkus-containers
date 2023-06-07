7f989370894d3aef4726a048d2697cd938b8fcc8
Stelio Kousouris <skousou@gmail.com>
2023-06-07 18:02:20 +0100

    quarkus-kafka-quickstart docs and helm charts for ocp deployment, test

--- quarkus-kafka-quickstart/rest-test/test.sh
+++ quarkus-kafka-quickstart/rest-test/test.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-NAMESPACE=$1
-DOMAIN=$2
+echo "------------------------------------------"
 
 curl -v -d "@event.json" \
     -H "Content-Type: application/json" \
-    -X POST https://quickstart-kafka-$NAMESPACE.$DOMAIN/event/in
-
-
-curl https://quickstart-kafka-$NAMESPACE.$DOMAIN/q/metrics | grep events
-
+    -X POST https://$(oc get route quickstart-kafka -o jsonpath='{.spec.host}')/event/in
+echo "------------------------------------------"
+echo
+echo "------------------------------------------"
+echo "https://$(oc get route quickstart-kafka -o jsonpath='{.spec.host}')/q/metrics | grep events"
+echo "------------------------------------------"
+echo
+curl https://$(oc get route quickstart-kafka -o jsonpath='{.spec.host}')/q/metrics | grep events
+echo "------------------------------------------"
